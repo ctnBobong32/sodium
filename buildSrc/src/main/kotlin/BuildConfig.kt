4175b018b9ed3c6262d2a1d2751fcf65a68557e3
ctnBobong32 <3647844952@qq.com>
2026-02-18 13:42:48 +0800

    汉化并禁用 PojavLauncher 启动检查，解决 Android 环境崩溃问题

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,10 +1,10 @@
  import org.gradle.api.Project
 
 object BuildConfig {
-    val MINECRAFT_VERSION: String = "1.21.11"
+    val MINECRAFT_VERSION: String = "1.21.8"
     val NEOFORGE_VERSION: String = "21.11.10-beta"
     val FABRIC_LOADER_VERSION: String = "0.18.1"
-    val FABRIC_API_VERSION: String = "0.140.0+1.21.11"
+    val FABRIC_API_VERSION: String = "0.140.0+1.21.8"
     val SUPPORT_FRAPI : Boolean = true
 
     // This value can be set to null to disable Parchment.
